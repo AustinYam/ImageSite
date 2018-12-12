8d290ea5b09b0af40e795102aac51b834f9eaf6f
Arselan Alvi <arselan95@gmail.com>
2018-12-11 19:19:23 -0800

    Update startScreen.php

--- startScreen.php
+++ startScreen.php
@@ -16,7 +16,59 @@ $query = "SELECT id from customer where userName='$tmp' and password='$p'";
 			}
 	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
 
-	
+if (isset($_POST['search']) && isset($_POST['enter']))
+{
+	$t=array();
+	$q=array();
+	$value=array();
+	$val="";
+	$category = get_post($conn, 'enter');
+	//echo $category;
+	echo "</br>";
+	//$query = "SELECT * FROM music WHERE category='$category'";
+	$query = "SELECT * FROM music";
+	$result = $conn->query($query);
+	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
+	$rows = $result->num_rows;
+	while ($row = $result->fetch_assoc()) {
+			
+		$q[]= $row['source'];
+		foreach($q as $value)
+		{
+			$value=strtolower(substr($value, 0, -4));
+			
+		}
+		$test[]=$value;
+		//echo $value;
+		//$val=$value;
+		
+		$t[]=$row['category'];
+		//echo $q;
+		
+		//print_r ($t);
+	}
+	echo "</br>";
+	//print_r($t);
+	//print_r($test);
+	//echo $category;
+	if(in_array($category, $t)!==false)
+	{
+
+	$query = "SELECT * FROM music WHERE category like '$category'";
+	$result = $conn->query($query);
+	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";	
+	}
+	if(in_array($category, $test)!==false)
+	{
+		//echo $category;
+		echo "</br>";
+		echo $val;
+	echo "yes";
+	$query = "SELECT * from music where LOWER(substr(source from 1 for char_length(source)-4)) = LOWER('$category')";
+	$result = $conn->query($query);
+	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";	
+	}
+}
 	
 	
 	if (isset($_POST['trending']))

a7b8f2bad3e29e3fcd0dacf0c80fb10253f69e3d
AustinYam <austin.yam@sjsu.edu>
2018-12-12 13:12:30 -0800

    upload update

--- cart.php
+++ cart.php
@@ -1,10 +1,16 @@
 <?php
-session_start();
 require_once 'login.php';
 $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die($conn->connect_error);
+
+require_once('authenticate.php');
+
+$_SESSION["tmpid"]="";
+$tmp= $_SESSION["user"];
+$p= $_SESSION["pass"];
+$tcustcre;
+
 $buyid= $_SESSION["tmpid"];
-echo "your id: ". $buyid;
  $total="";
  $query = "SELECT * from customer where id='$buyid'";
 	$result = $conn->query($query);
@@ -14,9 +20,6 @@ echo "your id: ". $buyid;
 			$tcustcre=$row['credits'];
 			}
 	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
-	echo"</br>";
-	echo "you have $".$tcustcre;
-	echo "</br>";
 /*
 if (isset($_POST['delete']) && isset($_POST['id']))
 {
@@ -189,3 +192,125 @@ _END;
 			}	
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css" integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h" crossorigin="anonymous">
+</head>
+<body>
+	<!--NAVBAR-->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  		<h2 class="display-5"><a class="navbar-brand" href="startScreen.php">Giggity</a></h2>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		 </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item">
+		        <a class="nav-link" href="startScreen.php">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="ImageTest.php">Upload</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="ItemsBought.php">Purchases</a>
+		      </li>   
+		      <li class="nav-item"> 
+			      <a class="nav-link" href="search.php">Search</a>
+			  </li>
+		       <form class="form-inline my-2 my-lg-0 ml-2" action="" method="post">
+					<button class="btn btn-outline-success" type="submit" name="trending" >Popular</button>
+			  </form>
+			  <li class="nav-item" style="position: absolute; right: 0">
+		      	<div>
+		      		<a href="cart.php"><button class="btn btn-success mr-2"><i class="fa fa-shopping-cart mr-2"></i>Cart</button></a>
+		      	</div>
+		      </li>
+		       <li class="nav-item mr-5" style="position: absolute; right: 0">
+		      	<div class="dropdown mr-5">
+		      		<button class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Welcome, <?= $tmp ?>
+		      		</button>
+		      		<div class="dropdown-menu">
+		      			<a class="dropdown-item" href="#">Wallet: $<?= $tcustcre ?></a>
+					    <div class="dropdown-divider"></div>
+					    <a class="dropdown-item" href="cart.php">Cart</a>
+					    <a class="dropdown-item" href="ItemsBought.php">Purchases</a>
+					    <a class="dropdown-item" href="ImageTest.php">Upload</a>
+					    <div class="dropdown-divider"></div>
+					    <a class="dropdown-item" href="logout.php">Logout</a>
+					</div>
+		      	</div>
+		      </li>
+		    </ul>
+		  </div>
+	</nav>
+	<h2 class="ml-2">Trending</h2>
+	<div class="row">
+		<?php
+			$query = "select * from transaction where transactionDate between '2018-12-11' and '2018-12-12'  group by source order by count(*) desc";
+	$result = $conn->query($query);
+	if (!$result) echo "Select failed: $query<br>" . $conn->error . "<br><br>";
+		while ($row = $result->fetch_assoc()) {
+			
+		$q= $row['source'];
+		$c[]=$row['category'];
+		$newq[]=substr_replace($q, 'new.png',-4);
+	}
+	//print_r($newq);
+	foreach ($newq as $k => $v) {
+	$query ="INSERT INTO trending VALUES(NULL,'$newq[$k]','$c[$k]')";
+	$result = $conn->query($query);
+	if (!$result) echo "INSERT failed: $query<br>" . $conn->error . "<br><br>";
+	
+	$query = "SELECT * FROM trending";
+		$result = $conn->query($query);
+		if (!$result) die ("Database access failed: " . $conn->error);
+		$rows = $result->num_rows;
+		
+		for ($j = 0 ; $j < $rows ; ++$j)
+		{
+			
+			$result->data_seek($j);
+			$row = $result->fetch_array(MYSQLI_NUM);
+			//$g=$row[3];
+			//echo '<img src="'.$g.'" alt="HTML5 Icon" style="width:128px;height:128px">';
+echo <<<_END
+
+
+<div class="column ml-2 mb-5 mt-2" style=float:left;padding:10 10 10 10>
+	<div class="card" style="width: 10rem;">
+	  <img class="card-img-top" height = "150" width = "42" src= $row[1] alt="HTML5 Icon">
+	  <div class="card-body">
+	    <form action="startScreen.php" method="post">
+			<input type="hidden" name="choose" value="yes">
+			<input type="hidden" name="source" value="$row[1]">
+			<input type="hidden" name="category" value="$row[2]">
+			<button class = "btn btn-outline-success" type="submit">Add to Cart</button>
+		</form>
+	  </div>
+	</div>
+
+
+</div>
+_END;
+
+			}
+				$query = "delete  FROM trending";
+		$result = $conn->query($query);
+		if (!$result) die ("Database access failed: " . $conn->error);	
+	}
+		?>
+		
+	</div>
+</body>
+<footer>
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</footer>
+</html>

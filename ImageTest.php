399d4578db459d12c4bd1d14a4636d178ce1cdb8
AustinYam <austin.yam@sjsu.edu>
2018-12-11 17:34:18 -0800

    Update ImageTest.php

--- ImageTest.php
+++ ImageTest.php
@@ -2,139 +2,21 @@
 require_once 'login.php';
 $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die($conn->connect_error);
-/*
-//search
-echo <<<_END
-<form action="" method="post">
 
-<input type="text" name="enter">
-search by<type="label" name="searchby">
-<select>
-<option name="category">category</option>
-<option value="name">name</option>
-<option value="size">size</option>
-</select>
-<input type="submit" name="search" value="SEARCH">
-
-</form>
-_END;
-
-if (isset($_POST['search']) && isset($_POST['enter']))
-{
-	$t=array();
-	$q=array();
-	$value=array();
-	$val="";
-	$category = get_post($conn, 'enter');
-	echo $category;
-	echo "</br>";
-	//$query = "SELECT * FROM music WHERE category='$category'";
-	$query = "SELECT * FROM music";
-	$result = $conn->query($query);
-	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
-	$rows = $result->num_rows;
-	while ($row = $result->fetch_assoc()) {
-			
-		$q[]= $row['source'];
-		foreach($q as $value)
-		{
-			$value=strtolower(substr($value, 0, -4));
-			
-		}
-		$test[]=$value;
-		//echo $value;
-		//$val=$value;
-		
-		$t[]=$row['category'];
-		//echo $q;
-		
-		//print_r ($t);
-	}
-	echo "</br>";
-	print_r($t);
-	print_r($test);
-	echo $category;
-	if(in_array($category, $t)!==false)
-	{
-
-	$query = "SELECT * FROM music WHERE category like '$category'";
-	$result = $conn->query($query);
-	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";	
-	$rows = $result->num_rows;
-	for ($j = 0 ; $j < $rows ; ++$j)
-		{
-			$result->data_seek($j);
-			$row = $result->fetch_array(MYSQLI_NUM);
-echo <<<_END
-<pre>
-id $row[0]
-resolution $row[1]
-size $row[2]
-source $row[3]
-category $row[4]
-</pre>
-<form action="ImageTest.php" method="post">
-<input type="hidden" name="delete" value="yes">
-<input type="hidden" name="id" value="$row[0]">
-<input type="submit" value="DELETE RECORD">
-</form>
-<form action="ImageTest.php" method="post">
-<input type="hidden" name="choose" value="yes">
-<input type="hidden" name="id" value="$row[0]">
-<input type="submit" value="CHOOSE RECORD">
-</form>
-_END;
-			}		
-	}
-	
-	echo $category;
-	 if(in_array($category, $test)!==false)
-	{
-		echo $category;
-		echo "</br>";
-		echo $val;
-	echo "yes";
-	$query = "SELECT * from music where LOWER(substr(source from 1 for char_length(source)-4)) = LOWER('$category')";
-	$result = $conn->query($query);
-	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";	
-	$rows = $result->num_rows;
-	for ($j = 0 ; $j < $rows ; ++$j)
-		{
-			$result->data_seek($j);
-			$row = $result->fetch_array(MYSQLI_NUM);
-echo <<<_END
-<pre>
-id $row[0]
-resolution $row[1]
-size $row[2]
-source $row[3]
-category $row[4]
-</pre>
-<form action="ImageTest.php" method="post">
-<input type="hidden" name="delete" value="yes">
-<input type="hidden" name="id" value="$row[0]">
-<input type="submit" value="DELETE RECORD">
-</form>
-<form action="ImageTest.php" method="post">
-<input type="hidden" name="choose" value="yes">
-<input type="hidden" name="id" value="$row[0]">
-<input type="submit" value="CHOOSE RECORD">
-</form>
-_END;
-			}		
-	}
-			
-	}
-	*/
 	//search 
-echo <<<_END
-<form action="search.php" method="post">
-<input type="hidden" name="choose" value="yes">
-<input type="submit" value="search images">
-</form>
-_END;
+require_once('authenticate.php');
 
+$_SESSION["tmpid"]="";
+$tmp= $_SESSION["user"];
+$p= $_SESSION["pass"];
 
+$query = "SELECT id from customer where userName='$tmp' and password='$p'";
+	$result = $conn->query($query);
+	while ($row = $result->fetch_assoc()) {
+			//echo "Welcome ".$tmp." , id: ".$row['id']."<br>";
+			$_SESSION["tmpid"]=$row['id'];
+			}
+	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
 //from sqltest example provided by professor
 //delete from database
 if (isset($_POST['delete']) && isset($_POST['id']))
@@ -342,8 +224,46 @@ _END;
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  		<a class="navbar-brand" href="#">Giggity</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		 </button>
 
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item">
+		        <a class="nav-link" href="startScreen.php">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="ImageTest.php">Browse</a>
+		      </li>
+		      <form class="form-inline my-2 my-lg-0 ml-5">
+			      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		    	  </form>
+		    </ul>
+		    <ul class="navbar-nav">
+		    	<form action="loginPage.php" method="post">
+		    		Welcome, <?= $tmp ?>
+			    	 <li class="nav-item">
+			    	 	<input type="hidden" name="choose" value="yes">
+				        <a class="nav-link" value="Log out" href="logout.php">Logout</a>
+				     </li>
+			     </form>
+		    </ul>
+		  </div>
+	</nav>
+	<div class="container">
+		<div class="row">
+			<form action="search.php" method="post">
+				<input type="hidden" name="choose" value="yes">
+				<input type="submit" value="search images">
+			</form>
+		</div>
+	</div>
 </body>
 </html>

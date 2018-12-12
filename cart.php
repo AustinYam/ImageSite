ca62c2744867a5d5ac1b0bfc1ed6549add7b5673
AustinYam <austin.yam@sjsu.edu>
2018-12-12 13:22:48 -0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,25 +1,13 @@
 <?php
 require_once 'login.php';
 $conn = new mysqli($hn, $un, $pw, $db);
-if ($conn->connect_error) die($conn->connect_error);
 
 require_once('authenticate.php');
-
-$_SESSION["tmpid"]="";
 $tmp= $_SESSION["user"];
 $p= $_SESSION["pass"];
-$tcustcre;
 
+if ($conn->connect_error) die($conn->connect_error);
 $buyid= $_SESSION["tmpid"];
- $total="";
- $query = "SELECT * from customer where id='$buyid'";
-	$result = $conn->query($query);
-	while ($row = $result->fetch_assoc()) {
-			//echo "Welcome ".$tmp." , id: ".$row['id']."<br>";
-			$ti=$row['id'];
-			$tcustcre=$row['credits'];
-			}
-	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
 /*
 if (isset($_POST['delete']) && isset($_POST['id']))
 {
@@ -30,24 +18,20 @@ if (isset($_POST['delete']) && isset($_POST['id']))
 	if (!$result) echo "DELETE failed: $query<br>" . $conn->error . "<br><br>";
 	}
 	*/
-	
 
 //delete from cart
-if (isset($_POST['delete']) && isset($_POST['id']) && isset($_POST['res']) && isset($_POST['size']) && isset($_POST['source']) && isset($_POST['category']) && isset($_POST['credits']))
+if (isset($_POST['delete']) && isset($_POST['id']) && isset($_POST['res']) && isset($_POST['size']) && isset($_POST['source']) && isset($_POST['category']))
 {
 	echo "hi";
 	$id = get_post($conn, 'id');
 	$res = get_post($conn, 'res');
 	$size = get_post($conn, 'size');
 	$name = get_post($conn, 'source');
-	$cat = get_post($conn, 'category');
-	$cre = get_post($conn, 'credits');
+		$cat = get_post($conn, 'category');
 	
-	$query = "INSERT INTO music VALUES('$id','$res','$size','$name','$cat','$cre')";
+	$query = "INSERT INTO music VALUES('$id','$res','$size','$name','$cat')";
 	$result = $conn->query($query);
 	if (!$result) die("Database access failed: ". $conn->error);
-	
-	$total=$total-$cre;
 	
 	$query = "DELETE FROM cart WHERE id='$id'";
 	$result = $conn->query($query);
@@ -61,45 +45,33 @@ if (isset($_POST['delete']) && isset($_POST['id']) && isset($_POST['res']) && is
 	
 	$query ="SELECT * FROM cart";
 		$result = $conn->query($query);
-		$tcred=array();
 		
 		while ($row = $result->fetch_assoc()) {
 		$s[]=$row['id'];
-		//print_r ($s);
+		print_r ($s);
 		$ts=$row['source'];
 			$newts[]=substr_replace($ts, '.jpg',-7);
-		$tc[]=$row['category'];
-		$tcred[]=$row['credits'];
+					
+		
+		
 	}
-	print_r ($tcred);
-	$sum= array_sum($tcred);
 		if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
-	$query = "SELECT * from customer where id='$buyid'";
+	$query = "SELECT id from customer where id='$buyid'";
 	$result = $conn->query($query);
 	while ($row = $result->fetch_assoc()) {
 			//echo "Welcome ".$tmp." , id: ".$row['id']."<br>";
 			$ti=$row['id'];
-			$tcustcre=$row['credits'];
 			}
 	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
-	echo "</br>";
-	echo $sum;
-	if($tcustcre > $sum)
-	{
-		$upcustcre=$tcustcre-$sum;
-		echo $upcustcre;
-		$query = "update customer set credits=$upcustcre where id='$buyid'";
-	$result = $conn->query($query);
-	if (!$result) echo "update failed: $query<br>" . $conn->error . "<br><br>";
 	
-
+	
 		foreach ($s as $k => $v) {
-    $query ="INSERT INTO transaction VALUES(NULL,'$ti','$s[$k]','$newts[$k]','$tc[$k]',CURDATE())";
+			//foreach($newts as $b => $bit){
+    $query ="INSERT INTO transaction VALUES(NULL,'$ti','$s[$k]','$newts[$k]',CURDATE())";
 	$result = $conn->query($query);
 	if (!$result) echo "INSERT failed: $query<br>" . $conn->error . "<br><br>";
-			
+			//}
 }
-
 
 	
 	$query = "DELETE FROM cart";
@@ -107,103 +79,21 @@ if (isset($_POST['delete']) && isset($_POST['id']) && isset($_POST['res']) && is
 	if (!$result) echo "DELETE failed: $query<br>" . $conn->error . "<br><br>";
 	
 	}
-	else{
-		echo "not enough credits bitch $$, delete few items you broke arse mfucker";
-		header("location: cart.php");
-	}
-	
-	}
 echo <<<_END
-<form action="cart.php" method="post">
-<input type="hidden" name="id" value="">
-<input type="hidden" name="buy" value="yes">
-<input type="submit" value="BUY">
-</form>
+
 _END;
-
-echo <<<_END
-<form action="startScreen.php" method="post">
-<input type="hidden" name="choose" value="yes">
-<input type="submit" value="Home">
-</form>
-_END;
-
-
- $query ="SELECT * FROM cart";
-		$result = $conn->query($query);
-		if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
-		echo "Cart: ";
-		echo "</br>";
-		while ($row = $result->fetch_assoc()) {
-			
-		$q= $row['id'];
-		$t=$row['resolution'];
-		$u=$row['size'];
-		$v=$row['source'];
-	}
-
-	
-	$query = "SELECT * FROM cart";
-		$result = $conn->query($query);
-		$tcred=array();
-		while ($row = $result->fetch_assoc()) {
-		$tcred[]=$row['credits'];
-	}
-	$total=array_sum($tcred);
-	echo "total: ".$total;
-		if (!$result) die ("Database access failed: " . $conn->error);
-		
-		$rows = $result->num_rows;
-		
-		for ($j = 0 ; $j < $rows ; ++$j)
-		{
-			$result->data_seek($j);
-			$row = $result->fetch_array(MYSQLI_NUM);
-echo <<<_END
-<div class=column style=float:left;padding:10 10 10 10>
-<pre>
-id $row[0]
-resolution $row[1]
-size $row[2]
-source $row[3]
-category: $row[4]
-credits: $row[5]
-<img src= $row[3] alt="HTML5 Icon" style="width:128px;height:128px">
-</pre>
-
-<form action="cart.php" method="post">
-<input type="hidden" name="delete" value="yes">
-<input type="hidden" name="id" value="$row[0]">
-<input type="hidden" name="res" value="$row[1]">
-<input type="hidden" name="size" value="$row[2]">
-<input type="hidden" name="source" value="$row[3]">
-<input type="hidden" name="category" value="$row[4]">
-<input type="hidden" name="credits" value="$row[5]">
-
-<input type="submit" value="DELETE RECORD">
-</form>
-</div>
-_END;
-			}
-			
-			$result->close();
-			$conn->close();
-			function get_post($conn, $var){return $conn->real_escape_string($_POST[$var]);
-			}	
-
 ?>
 
 <!DOCTYPE html>
 <html>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css" integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h" crossorigin="anonymous">
 <head>
 	<title></title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css" integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h" crossorigin="anonymous">
 </head>
 <body>
-	<!--NAVBAR-->
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  		<h2 class="display-5"><a class="navbar-brand" href="startScreen.php">Giggity</a></h2>
+  		<a class="navbar-brand" href="startScreen">Giggity</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		 </button>
@@ -235,8 +125,6 @@ _END;
 		      		<button class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Welcome, <?= $tmp ?>
 		      		</button>
 		      		<div class="dropdown-menu">
-		      			<a class="dropdown-item" href="#">Wallet: $<?= $tcustcre ?></a>
-					    <div class="dropdown-divider"></div>
 					    <a class="dropdown-item" href="cart.php">Cart</a>
 					    <a class="dropdown-item" href="ItemsBought.php">Purchases</a>
 					    <a class="dropdown-item" href="ImageTest.php">Upload</a>
@@ -248,66 +136,71 @@ _END;
 		    </ul>
 		  </div>
 	</nav>
-	<h2 class="ml-2">Trending</h2>
 	<div class="row">
 		<?php
-			$query = "select * from transaction where transactionDate between '2018-12-11' and '2018-12-12'  group by source order by count(*) desc";
-	$result = $conn->query($query);
-	if (!$result) echo "Select failed: $query<br>" . $conn->error . "<br><br>";
+			 $query ="SELECT * FROM cart";
+		$result = $conn->query($query);
+		if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
+		echo "</br>";
 		while ($row = $result->fetch_assoc()) {
 			
-		$q= $row['source'];
-		$c[]=$row['category'];
-		$newq[]=substr_replace($q, 'new.png',-4);
+		$q= $row['id'];
+		$t=$row['resolution'];
+		$u=$row['size'];
+		$v=$row['source'];
 	}
-	//print_r($newq);
-	foreach ($newq as $k => $v) {
-	$query ="INSERT INTO trending VALUES(NULL,'$newq[$k]','$c[$k]')";
-	$result = $conn->query($query);
-	if (!$result) echo "INSERT failed: $query<br>" . $conn->error . "<br><br>";
+
 	
-	$query = "SELECT * FROM trending";
+	$query = "SELECT * FROM cart";
 		$result = $conn->query($query);
 		if (!$result) die ("Database access failed: " . $conn->error);
 		$rows = $result->num_rows;
 		
 		for ($j = 0 ; $j < $rows ; ++$j)
 		{
-			
 			$result->data_seek($j);
 			$row = $result->fetch_array(MYSQLI_NUM);
-			//$g=$row[3];
-			//echo '<img src="'.$g.'" alt="HTML5 Icon" style="width:128px;height:128px">';
 echo <<<_END
-
-
-<div class="column ml-2 mb-5 mt-2" style=float:left;padding:10 10 10 10>
-	<div class="card" style="width: 10rem;">
-	  <img class="card-img-top" height = "150" width = "42" src= $row[1] alt="HTML5 Icon">
+<div class="column ml-2 mb-5" style=float:left;padding:10 10 10 10>
+	<div class="card" style="width: 15rem;">
+	  <img class="card-img-top" src= $row[3] alt="HTML5 Icon">
 	  <div class="card-body">
-	    <form action="startScreen.php" method="post">
-			<input type="hidden" name="choose" value="yes">
-			<input type="hidden" name="source" value="$row[1]">
-			<input type="hidden" name="category" value="$row[2]">
-			<button class = "btn btn-outline-success" type="submit">Add to Cart</button>
+	    <h5 class="card-title">$row[3]</h5>
+	    <p class="card-text">id: $row[0]</p>
+	    <p class="card-text">resolution: $row[1]</p>
+	    <p class="card-text">size: $row[2]</p>
+	    <p class="card-text">category: $row[4]</p>
+	  
+		<form class = "mt-2" action="ImageTest.php" method="post">
+			<input type="hidden" name="delete" value="yes">
+			<input type="hidden" name="id" value="$row[0]">
+			<input type="hidden" name="res" value="$row[1]">
+			<input type="hidden" name="size" value="$row[2]">
+			<input type="hidden" name="source" value="$row[3]">
+			<input type="hidden" name="category" value="$row[4]">
+			<button class = "btn btn-outline-success" type="submit">Remove</button>
 		</form>
 	  </div>
 	</div>
-
-
-</div>
 _END;
-
 			}
-				$query = "delete  FROM trending";
-		$result = $conn->query($query);
-		if (!$result) die ("Database access failed: " . $conn->error);	
-	}
+			
+			$result->close();
+			$conn->close();
+			function get_post($conn, $var){return $conn->real_escape_string($_POST[$var]);
+			}	
 		?>
-		
+	</div>
+	<div class="mr-5 mb-5" style="position: absolute; right: 0">
+		<form action="cart.php" method="post">
+			<input type="hidden" name="id" value="">
+			<input type="hidden" name="buy" value="yes">
+			<button class="btn btn-outline-success" type="submit">Purchase</button>
+		</form>
 	</div>
 </body>
 <footer>
+
 	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>

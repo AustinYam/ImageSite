59b70840e533e36af5d4a96146050b5e129db91c
Arselan Alvi <arselan95@gmail.com>
2018-12-13 12:07:42 -0800

    changed name

--- authenticate.php
+++ authenticate.php
@@ -1,4 +1,8 @@
+
 <?php
+/*
+by Austin
+*/
 session_start();
 if(empty($_SESSION["authenticated"]) || $_SESSION["authenticated"] != 'true') {
     header('Location: loginPage.php');
--- cart.php
+++ cart.php
@@ -103,7 +103,7 @@ _END;
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  		<a class="navbar-brand" href="startScreen">Giggity</a>
+  		<a class="navbar-brand" href="startScreen">Aurora</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		 </button>

1ea7ff13fd0e4bf3d97833ec8bc505cd03329da3
AustinYam <austin.yam@sjsu.edu>
2018-12-12 17:47:24 -0800

    Update searchTransaction.php

--- searchTransaction.php
+++ searchTransaction.php
@@ -2,13 +2,13 @@
 require_once 'login.php';
 $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die($conn->connect_error);
+
 require_once('authenticate.php');
 
+$_SESSION["tmpid"]="";
 $tmp= $_SESSION["user"];
 $p= $_SESSION["pass"];
-$boughtid= $_SESSION["tmpid"];
 
-if (isset($_POST['search']) && isset($_POST['enter']))
 
 //search
 echo <<<_END
@@ -201,10 +201,6 @@ div#columns:hover figure:not(:hover) {
 	$value=array();
 	$val="";
 	$category = get_post($conn, 'enter');
-	//echo $category;
-	echo "</br>";
-	$query = "SELECT * FROM transaction where customerID='$boughtid'";
-
 
 	$query = "SELECT * FROM transaction";
 	$result = $conn->query($query);
@@ -219,18 +215,13 @@ div#columns:hover figure:not(:hover) {
 			
 		}
 		$test[]=$value;
-		//echo $value;
-		//$val=$value;
+
 		
 		$t[]=$row['category'];
-		//echo $q;
-		
-		//print_r ($t);
+
 	}
 	echo "</br>";
-	//print_r($t);
-	//print_r($test);
-	//echo $category;
+
 	if(in_array($category, $t)!==false)
 	{
 
@@ -243,24 +234,6 @@ div#columns:hover figure:not(:hover) {
 			$result->data_seek($j);
 			$row = $result->fetch_array(MYSQLI_NUM);
 echo <<<_END
-<div class=column style=float:left;padding:10 10 10 10>
-<pre>
-orderNumber $row[0]
-customerID $row[1]
-imageID $row[2]
-source $row[3]
-transactionDate $row[4]
-<img src= $row[3] alt="HTML5 Icon" style="width:128px;height:128px">
-</pre>
-<form action="cart.php" method="post">
-<input type="hidden" name="delete" value="yes">
-<input type="hidden" name="orderNumber" value="$row[0]">
-<input type="hidden" name="customerID" value="$row[1]">
-<input type="hidden" name="imageID" value="$row[2]">
-<input type="hidden" name="source" value="$row[3]">
-<input type="hidden" name="transactionDate" value="$row[4]">
-<input type="submit" value="DELETE RECORD">
-</form>
 <figure>
 <div class="card mt-5 ml-3">
 	<img class = "card-image-top" src= $row[3] alt="HTML5 Icon" style="width:128px;height:128px">
@@ -292,9 +265,7 @@ _END;
 		//echo $category;
 		echo "</br>";
 		echo $val;
-	echo "yes";
-	$query = "SELECT * from transaction where LOWER(substr(source from 1 for char_length(source)-4)) like LOWER('$category') and customerID='$boughtid'";
-	//$query = "SELECT * from transaction where LOWER(substr(source from 1 for char_length(source)-4)) like LOWER('$category')";
+	$query = "SELECT * from transaction where LOWER(substr(source from 1 for char_length(source)-4)) like LOWER('$category')";
 	$result = $conn->query($query);
 	if (!$result) echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";	
 	$rows = $result->num_rows;
@@ -303,24 +274,6 @@ _END;
 			$result->data_seek($j);
 			$row = $result->fetch_array(MYSQLI_NUM);
 echo <<<_END
-<div class=column style=float:left;padding:10 10 10 10>
-<pre>
-orderNumber $row[0]
-customerID $row[1]
-imageID $row[2]
-source $row[3]
-transactionDate $row[4]
-<img src= $row[3] alt="HTML5 Icon" style="width:128px;height:128px">
-</pre>
-<form action="itemsBought.php" method="post">
-<input type="hidden" name="delete" value="yes">
-<input type="hidden" name="orderNumber" value="$row[0]">
-<input type="hidden" name="customerID" value="$row[1]">
-<input type="hidden" name="imageID" value="$row[2]">
-<input type="hidden" name="source" value="$row[3]">
-<input type="hidden" name="transactionDate" value="$row[4]">
-<input type="submit" value="DELETE RECORD">
-</form>
 <figure>
 <div class="card mt-5 ml-3">
 	<img class = "card-image-top" src= $row[3] alt="HTML5 Icon" style="width:128px;height:128px">
@@ -361,66 +314,15 @@ $query = "SELECT * FROM music";
 			$conn->close();
 			function get_post($conn, $var){return $conn->real_escape_string($_POST[$var]);
 			}	
-			
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css" integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h" crossorigin="anonymous">
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  		<a class="navbar-brand" href="startScreen">Giggity</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		 </button>
 
-		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		    <ul class="navbar-nav mr-auto">
-		      <li class="nav-item">
-		        <a class="nav-link" href="startScreen.php">Home <span class="sr-only">(current)</span></a>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link" href="ImageTest.php"><?= $tmp ?>'s Wall</a>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link" href="ItemsBought.php">Purchases</a>
-		      </li>   
-		      <li class="nav-item"> 
-			      <a class="nav-link" href="search.php">Search</a>
-			  </li>
-		       <form class="form-inline my-2 my-lg-0 ml-2" action="" method="post">
-					<button class="btn btn-outline-success" type="submit" name="trending" >Popular</button>
-			  </form>
-		        <li class="nav-item" style="position: absolute; right: 0">
-		      	<div>
-		      		<a href="cart.php"><button class="btn btn-success mr-2"><i class="fa fa-shopping-cart mr-2"></i>Cart</button></a>
-		      	</div>
-		      </li>
-		       <li class="nav-item mr-5" style="position: absolute; right: 0">
-		      	<div class="dropdown mr-5">
-		      		<button class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Welcome, <?= $tmp ?>
-		      		</button>
-		      		<div class="dropdown-menu">
-					    <a class="dropdown-item" href="cart.php">Cart</a>
-					    <a class="dropdown-item" href="ItemsBought.php">Purchases</a>
-					    <a class="dropdown-item" href="ImageTest.php">Upload</a>
-					    <div class="dropdown-divider"></div>
-					    <a class="dropdown-item" href="logout.php">Logout</a>
-					</div>
-		      	</div>
-		      </li>
-		    </ul>
-		  </div>
-	</nav>
-	<div class="container-fluid">
-		<form action="" method="post">
-			<input class="mt-2" type="search" name="enter">
-			<type="label" name="searchby">
-			<button class="btn btn-outline-success" type="submit" name="search">Search</button>
-		</form>
+		?>
 	</div>
 </body>
+<footer>
+
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</footer>
 </html>

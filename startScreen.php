6e4debe35d977edb5926a3b273f0a5ff7b7e98e9
Austin Yam <austin.yam123@gmail.com>
2018-12-13 20:26:18 -0800

    update

--- startScreen.php
+++ startScreen.php
@@ -303,6 +303,7 @@ div#columns:hover figure:not(:hover) {
 					    <a class="dropdown-item" href="ItemsBought.php">Purchases</a>
 					    <a class="dropdown-item" href="ImageTest.php">Upload</a>
 					    <div class="dropdown-divider"></div>
+					    <a class="dropdown-item" href="adminProfile.php">Admin</a>
 					    <a class="dropdown-item" href="logout.php">Logout</a>
 					</div>
 		      	</div>
